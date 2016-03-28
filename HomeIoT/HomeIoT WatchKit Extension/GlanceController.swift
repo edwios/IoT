f2afea06b52fc2540c53a2dc3231f606aebdf972
Edwin Tam <edwintam@iostation.com>
2016-03-29 03:01:54 +0800

    Initial checkin

---
+++ HomeIoT/HomeIoT WatchKit Extension/GlanceController.swift
@@ -0,0 +1,31 @@
+//
+//  GlanceController.swift
+//  HomeIoT WatchKit Extension
+//
+//  Created by Edwin Tam on 27/3/16.
+//  Copyright © 2016 Edwin Tam. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class GlanceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
