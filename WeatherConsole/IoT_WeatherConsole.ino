be874458782fd26288df21efeea2e23ba05c8ebb
Edwin Tam <edwintam@iostation.com>
2017-04-02 21:27:20 +0800

    Improved wireless reliability
    Improved wireless connection reliability including auto-retry.
    Rounded all decimal numbers into integers.
    Date time now subscribed to MQTT message instead of getting from the Internet.

--- WeatherConsole/IoT_WeatherConsole.ino
+++ WeatherConsole/IoT_WeatherConsole.ino
@@ -3,6 +3,36 @@
 #include "MQTT/MQTT.h"
 
 #define version 100
+/*
+ * This is a minimal example, see extra-examples.cpp for a version
+ * with more explantory documentation, example routines, how to 
+ * hook up your pixels and all of the pixel types that are supported.
+ *
+ */
+
+/*
+ * JSON parser example
+   StaticJsonBuffer<200> jsonBuffer;
+
+  char json[] =
+      "{\"sensor\":\"gps\",\"time\":1351824120,\"data\":[48.756080,2.302038]}";
+
+  JsonObject& root = jsonBuffer.parseObject(json);
+
+  if (!root.success()) {
+    Serial.println("parseObject() failed");
+    return;
+  }
+
+  const char* sensor = root["sensor"];
+  long time = root["time"];
+  double latitude = root["data"][0];
+  double longitude = root["data"][1];
+  
+ *
+ */
+  
+SYSTEM_MODE(MANUAL);
 
 #define CLIENT_NAME "envconsole01"
 
@@ -15,6 +45,9 @@
 #define TP_BALTEMP  "/balcony/temperature"
 #define TP_BALHUMI  "/balcony/humidity"
 #define TP_STATUS   "sensornet/status/envconsole01"
+#define TP_DATESTR "sensornet/time/shortdate"
+#define TP_TIMESTR "sensornet/time/shorttime"
+
 #define ARMED    "ARMED"
 #define DISARMED   "DISARMED"
 #define CMD_DISP_OFF "display/off"
@@ -26,8 +59,10 @@
 #define HBFLASHTIME 250         // How fast do I blink normally
 #define RGBLEDRELASETIME 3500   // 3.5s to release control of RBG LED
 #define LCDRELASETIME 15000     // 15s to release LCD
-
+#define LCDREFRESHTIME 60000    // 1 min data refresh time
 #define RECONNECT 10000         // Try reconnect every 10s
+#define FAST 125
+#define SLOW 500
 
 #define LCD_TEXT 1
 #define LCD_VALUE 2
@@ -56,7 +91,6 @@ void callback(char* topic, byte* payload, unsigned int length);
 
 // JSON Parser
 
-// MQTT client("stoy.iostation.com", 1883, callback);
 byte server[] = { 10,0,1,250 };
 MQTT client(server, 1883, callback);
 
@@ -67,11 +101,17 @@ String tp_baltemp = TP_BALTEMP;
 String tp_balhumi = TP_BALHUMI;
 String tp_status = TP_STATUS;
 
-String livtemp,livhumi,baltemp,balhumi,sstatus="-";
+String livtemp="-",livhumi="-",livaqi="-",baltemp="-",balhumi="-",balaqi="-",sstatus="-",timeStr="00:00", dateStr="Mon 00";
 
 // timers
-unsigned int ledTimer, lcdTimer, hbTimer, lastConnect;
+unsigned int d7Timer, ledTimer, lcdTimer, hbTimer, lastConnect;
+int d7Rate = 250;
+
+// flags
+bool d7On = false;
+bool init = true;
 bool blink;
+bool virgin=true;
 bool connected=false;
 bool armed=false;
 
@@ -91,18 +131,22 @@ void callback(char* topic, byte* payload, unsigned int length) {
     m = String(p);
     if (tp.endsWith(TP_LIVTEMP)) {
         // Living room temperature
+        m = String(round(m.toFloat()+0.5));
         m.concat("C");
         livtemp = m;
     } else if (tp.endsWith(TP_LIVHUMI)) {
         // Living room temperature
+        m = String(round(m.toFloat()+0.5));
         m.concat("%");
         livhumi = m;
     } else if (tp.endsWith(TP_BALHUMI)) {
         // Living room temperature
-        m.concat("%rH");
+        m = String(round(m.toFloat()+0.5));
+        m.concat("%");
         balhumi = m;
     } else if (tp.endsWith(TP_BALTEMP)) {
         // Living room temperature
+        m = String(round(m.toFloat()+0.5));
         m.concat("C");
         baltemp = m;
     } else if (tp.startsWith("sensornet/status/")) {
@@ -127,6 +171,10 @@ void callback(char* topic, byte* payload, unsigned int length) {
         } else if (tp.endsWith(CMD_DISP_ON)) {
             lcdOn();
         }
+    } else if (tp.equals(TP_DATESTR)) {
+        dateStr = m;
+    } else if (tp.equals(TP_TIMESTR)) {
+        timeStr = m;
     }
     updateAll();
 }
@@ -139,6 +187,9 @@ void updateAll()
     sendToLCD(LCD_TEXT,OUTDOORHUMIFIELD,balhumi);
     sendToLCD(LCD_TEXT,OUTDOORTEMPFIELD,baltemp);
     sendToLCD(LCD_TEXT,STATUSFIELD,sstatus);
+    sendToLCD(LCD_TEXT, CLOCKFIELD, timeStr);
+    sendToLCD(LCD_TEXT, DATEFIELD, dateStr);
+
     if (armed)
         sendToLCD(LCD_TEXT,ALARMFIELD,"ARM");
     else
@@ -229,6 +280,8 @@ void setMqtt(void)
     client.subscribe(TP_HOME);
     client.subscribe(TP_HOME_ALARM);
     client.subscribe(TP_COMMAND);
+    client.subscribe(TP_DATESTR);
+    client.subscribe(TP_TIMESTR);
 }
 
 bool updateBkgPict(void)
@@ -310,48 +363,85 @@ void setup()
 
 
 
-
 void loop() 
 {
-    char cTime[10];
+//    char cTime[10];
 
     // Maintain connection
-    if (client.isConnected()) {
-        client.loop();
-        connected = true;
-        sstatus = "Connected";
-    } else {
-        connected = false;
-        sstatus = "Not connected";
-        if (millis() - lastConnect > RECONNECT) {
-            // connect to the server
-            client.connect(CLIENT_NAME);
-            lastConnect = millis();
-            if (client.isConnected()) {
-                setMqtt();
-            } 
-        }
-    }
-
     if (millis() - ledTimer > RGBLEDRELASETIME) {
         ledTimer = millis();
         RGB.control(false);
+        d7Rate = SLOW;
+    }
+    
+    if (WiFi.ready() && init) {
+        // connect to the server
+        client.connect(CLIENT_NAME);
+        delay(500);
+        // publish/subscribe
+        setMqtt();
+        delay(200);
+        init = false;
     }
 
-    if (millis() - hbTimer > (connected?HBFLASHTIME:ERRFLASHTIME)) {
-        if (updateBkgPict()) updateAll();
-        sprintf(cTime, "%02d:%02d:%02d", Time.hour(), Time.minute(), Time.second());
-        sendToLCD(LCD_TEXT, CLOCKFIELD, String(cTime));
-        sendToLCD(LCD_TEXT, DATEFIELD, Time.format(Time.now(),"%h %d"));
-        hbTimer = millis();
-        blink = !blink;
-        if (blink) {
-            digitalWrite(HBLED, HIGH);
+    // Flash D7 according to the statue (controlled by d7Rate)
+    if (millis() - d7Timer > d7Rate) {
+        d7Timer = millis();
+        if (d7On) {
+            digitalWrite(HBLED,LOW);
         } else {
-            digitalWrite(HBLED, LOW);
+            digitalWrite(HBLED,HIGH);
         }
-        
+        d7On = !d7On;
+
+        if (client.isConnected()) {
+            client.loop();
+            connected = true;
+            sstatus = "Connected";
+        } else if (WiFi.ready()) {
+            connected = false;
+            sstatus = "Not connected";
+            RGB.control(true);
+            RGB.color(255,0,0);
+            d7Rate = FAST;
+            ledTimer = millis();
+            if (millis() - lastConnect > RECONNECT) {
+                lastConnect = millis();
+
+                RGB.color(40,0,0);
+                WiFi.off();
+                digitalWrite(D7, HIGH);
+                delay(1000);
+                WiFi.on();
+                RGB.color(255,255,0);
+                delay(500);
+                WiFi.connect();
+                RGB.color(0,0,255);
+                delay(500);
+                digitalWrite(D7,LOW);
+                init = true;
+            }
+        } else {
+            RGB.control(true);
+            RGB.color(255,0,255);
+            ledTimer = millis();
+        }
+        sendToLCD(LCD_TEXT,STATUSFIELD,sstatus);
     }
+
+
+/*
+    if (millis() - hbTimer > (connected?HBFLASHTIME:ERRFLASHTIME)) {
+        hbTimer = millis();
+    }
+*/
+    
+    if (virgin || (millis() - lcdTimer > LCDREFRESHTIME)) {
+        lcdTimer = millis();
+        virgin = false;         // Only once per life time ;)
+        updateAll();
+//        sprintf(cTime, "%02d:%02d", Time.hour(), Time.minute());
+        sendToLCD(LCD_TEXT,STATUSFIELD,sstatus);
+    }
+
 }
-
-
